498060731d2b92a7557ee24773bbcfc4a58563af
Linu Ramachandran <linuram@gmail.com>
2019-05-19 23:58:36 -0400

    added sidenav

--- javascript/app.js
+++ javascript/app.js
@@ -9,6 +9,22 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  
 setTimeout(function() {
   $("#icon").addClass("scale-in");
 }, 1000);
