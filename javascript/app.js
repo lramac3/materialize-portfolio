de719f5a57a68049fa730163a5dbdbb885e2caeb
Linu Ramachandran <linuram@gmail.com>
2019-05-20 10:24:02 -0400

    updated skills

--- javascript/app.js
+++ javascript/app.js
@@ -28,6 +28,24 @@ let elems2 = document.querySelectorAll('.carousel');
 //   numVisible: 2,
 //   indicators: true
 // })
+$('#owl-2 .owl-carousel').owlCarousel({
+  items: 5,
+  margin: 16,
+  stagePadding: 32,
+  loop: true,
+  autoplay: true,
+  autoplaySpeed: 500,
+  navSpeed: 500,
+  dots: false,
+  dotsEach: true,
+  nav: true,
+  // navText: ['Back','Next'],
+  navText: ['', ''],
+  autoplayTimeout: 2000,
+  autoplayHoverPause: false,
+  animateOut: 'slideOutDown',
+  animateIn: 'flipInX'
+})
 
 
   });
