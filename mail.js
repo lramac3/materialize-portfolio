438181777089cfc3d5dbe64626dad0b32b34ce13
Linu Ramachandran <linuram@gmail.com>
2019-05-24 12:01:59 -0400

    updated contact form

--- mail.js
+++ mail.js
@@ -3,8 +3,8 @@ const mailGun=require('nodemailer-mailgun-transport');
 
 const auth = {
   auth: {
-    api_key: process.env.MAILGUN_API_KEY||"21e88ed267bd643f766809f2f31798b9-52b0ea77-3dd6f133",
-    domain: process.env.MAILGUN_DOMAIN||"sandbox693755ca129749139807d0b89701b471.mailgun.org"
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
   }
 };
 
@@ -13,7 +13,7 @@ const transporter=nodemailer.createTransport(mailGun(auth));
 const sendMail= (email, subject, text, cb) => {
      const mailOptions = {
        from: email,
-       to: "linuram@gmail.com",
+       to: "linuram@protonmail.ch",
        subject,
        text
      };
