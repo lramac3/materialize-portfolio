65adea1827b9c771f851ef22c41249d6ae73eb98
Linu Ramachandran <linuram@gmail.com>
2019-05-24 11:47:55 -0400

    updated contact form

--- mail.js
+++ mail.js
@@ -3,8 +3,8 @@ const mailGun=require('nodemailer-mailgun-transport');
 
 const auth = {
   auth: {
-    api_key: "21e88ed267bd643f766809f2f31798b9-52b0ea77-3dd6f133",
-    domain: "sandbox693755ca129749139807d0b89701b471.mailgun.org"
+    api_key: process.env.MAILGUN_API_KEY||"21e88ed267bd643f766809f2f31798b9-52b0ea77-3dd6f133",
+    domain: process.env.MAILGUN_DOMAIN||"sandbox693755ca129749139807d0b89701b471.mailgun.org"
   }
 };
 
