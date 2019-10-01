9c038ae317c84ca7c0407c44cf7bcf1c3cb60f9f
Linu Ramachandran <linuram@gmail.com>
2019-10-01 18:51:46 -0400

    added preloader spinner

--- views/javascript/app.js
+++ views/javascript/app.js
@@ -93,3 +93,13 @@ $(function () {
     // }
   )
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('.preloader-background')
+    .delay(1700)
+    .fadeOut('slow')
+
+  $('.preloader-wrapper')
+    .delay(1700)
+    .fadeOut()
+})
